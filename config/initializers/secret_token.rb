eb6e37a5890c1b6c883ae2bcb2b2d4741523886b
Jacob Morris <jacob.s.morris@gmail.com>
2012-05-26 11:02:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolst::Application.config.secret_token = '02bab5d21b3cc592343bccf9b9cb71967a6295c25799ce8503fe50bd7c544cbcd4c976beafd43b32a21fe14f04871fc2651ed435e24f945b848d458f25cc2f06'
